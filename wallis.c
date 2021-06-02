b82db03e0a23d635580337a27fac05b6a179cfba
Kapil Agrawal <48560461+pahiya@users.noreply.github.com>
2021-06-02 20:51:15 +0530

    update

--- wallis.c
+++ wallis.c
@@ -7,15 +7,14 @@ float wallis_pi(int);
 
 int main(void) {
   float pi;
-  for (int i=1; i<5; i++) {
+  for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
+     
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
-  }
-
-  for (int i=500; i<3000; i++) {
+    for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) < 0.01)) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
@@ -23,17 +22,21 @@ int main(void) {
     }
   }
 }
+
+  
+}
 float wallis_pi (int n )
 {
   float pi=1;
   float a,b,comp_pi;
    
-   for(int i=1;i<n+1;i++)
+   for(int i=1;i<n;i++)
   {
     a= 4*i*i;
     b=a/(a-1);
   
     pi = pi*b;
+   
   }
   return (2*pi);
     }
